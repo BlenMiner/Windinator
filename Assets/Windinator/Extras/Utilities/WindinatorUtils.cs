8a10d4c3faedab92193e7b232535862dfcba7c92
Shadow <blenminer123@gmail.com>
2022-06-15 12:04:44 +0200

    some file refactorign

--- Assets/Windinator/Extras/Utilities/WindinatorUtils.cs
+++ Assets/Windinator/Extras/Utilities/WindinatorUtils.cs
@@ -4,24 +4,6 @@ using UnityEngine;
 
 public static class WindinatorUtils
 {
-    public static Vector3 WorldToCanvasPosition(this Canvas canvas, Vector3 worldPosition)
-    {
-        Vector3 Return = Vector3.zero;
-        var _Cam = canvas.worldCamera;
-
-        if (canvas.renderMode == RenderMode.ScreenSpaceOverlay)
-        {
-            Return = _Cam.WorldToScreenPoint(worldPosition);
-        }
-        else if (canvas.renderMode == RenderMode.ScreenSpaceCamera)
-        {
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, _Cam.WorldToScreenPoint(worldPosition), _Cam, out var tempVector);
-            Return = canvas.transform.TransformPoint(tempVector);
-        }
-
-        return Return;
-    }
-
     public static Vector3 ScreenToCanvasPosition(this Canvas canvas, Vector2 screenPosition)
     {
         return ScreenToCanvasPosition(canvas, canvas.transform as RectTransform, screenPosition);
